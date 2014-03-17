9dbaaaf69ce23df26c9ff32e99518808b47855d1
Xiangyu Bu <xybu92@live.com>
2014-03-17 01:24:16 -0400

    Explicit caching mechanism for group model and its evolvement.

--- app/models/Group.php
+++ app/models/Group.php
@@ -10,23 +10,51 @@ namespace models;
 
 class Group extends \Model {
 	
+	const MAX_ALIAS_LENGTH = 32;
+	const MAX_DESC_LENGTH = 150;
+	const GROUP_RECORD_TTL = 1800; //sec
+	
 	/**
 	 * isValidGroupName
 	 * Check if the string contains chars other than alphanumerical, -, _
 	 * and non-empty, and length not greater than 65.
 	 */
-	static function isValidGroupName($str){
+	static function isValidAlias($str){
 		if ($str === "") return false;
 		if (preg_match('/[^\-_A-Za-z0-9]/', $str)) return false;
-		if (strlen($str) < 65) return false;
+		if (strlen($str) > static::MAX_ALIAS_LENGTH) return false;
 		return true;
 	}
 	
+	static function isValidVisibility($v){
+		return is_numeric($v) and $v >= 0 and $v < 65;
+	}
+	
+	static function filterDescription($str){
+		$str = htmlspecialchars($str);
+		$str = substr($str, 0, static::MAX_DESC_LENGTH);
+		//$str = filter_var($str, FILTER_SANITIZE_SPECIAL_CHARS);
+		//$str = filter_var($str, FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FLAG_ENCODE_HIGH);
+		
+		return $str;
+	}
+	
+	static function filterTags($str){
+		return preg_replace("[^A-Za-z0-9 ]", "", strtolower($str));
+	}
+	
 	function findByGroupId($id){
-		$result = $this->queryDb("SELECT * FROM groups WHERE id=?;", $id, 1800);
-		if (count($result) == 1)
-			return $result[0];
-		return null;
+		if ($this->cache->exists("group_id_" . $id))
+			return $this->cache->get("group_id_" . $id);
+			
+		$result = $this->queryDb("SELECT * FROM groups WHERE id=?;", $id);
+		if (count($result) == 1){
+			$result = $result[0];
+			$result["users"] = json_decode($result["users"], true);
+			$this->cache->set("group_id_" . $id, $result, static::GROUP_RECORD_TTL);
+		} else $result = null;
+		
+		return $result;
 	}
 	
 	function findByGroupAlias($alias){
@@ -45,10 +73,32 @@ class Group extends \Model {
 	}
 	
 	function listGroupsOfUserId($user_id, $visibility = 0){
-		$result = $this->queryDb("SELECT * FROM groups WHERE visibility>=? AND users LIKE '%\"" . $user_id . "\"%';", $visibility, 1800);
+		$ids = $this->queryDb("SELECT id FROM groups WHERE users LIKE '%\"" . $user_id . "\"%';");
+		$result = array();
+		foreach ($ids as $i => $d){
+			$g_data = $this->findByGroupId($d["id"]);
+			if ($g_data["visibility"] >= $visibility)
+				$result[] = $g_data;
+		}
+		
 		if (count($result) > 0)
-			return $result;
-		return null;
+			return array("count" => count($result), "groups" =>$result);
+		return array("count" => 0);
+	}
+	
+	function create($user_id, $name, $desc, $tags, $visibility){
+		$this->queryDb(
+			"INSERT INTO groups (creator_user_id, visibility, alias, description, tags, users, created_at) " .
+			"VALUES (:user_id, :visibility, :alias, :desc, :tags, :users, NOW()); ",
+			array(
+				':user_id' => $user_id,
+				':visibility' => $visibility,
+				':alias' => $name,
+				':desc' => $desc,
+				':tags' => $tags,
+				':users' => json_encode(array("admin" => array($user_id)))
+			)
+		);
 	}
 	
 	function updateGroupProfile($id, $visibility, $alias, $description, $tags){
@@ -62,9 +112,26 @@ class Group extends \Model {
 				":tags" => $tags,
 			)
 		);
+		$this->cache->clear("group_id_" . $id);
 	}
 	
-	function updateCreatorUserId($gid, $uid){
+	function deleteById($gid){
+		//TODO: delete all records related to the group before deleting it
+		
+		// delete all the news about the group
+		$news = new News();
+		$news->deleteByGroupId($gid);
+		
+		// delete the group
+		$this->queryDb("DELETE FROM groups WHERE id=?;", $gid);
+		$this->cache->clear("group_id_" . $id);
+	}
+	
+	function setStatus($isActive = false){
+		
+	}
+	
+	function changeCreatorUserId($gid, $uid){
 		$this->queryDb(
 			"UPDATE groups SET creator_user_id=:uid WHERE id=:gid LIMIT 1;",
 			array(
