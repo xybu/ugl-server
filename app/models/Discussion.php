03a31cd3d9e06c4f0f8fc95713bf8195606f3125
Xiangyu Bu <xybu92@live.com>
2014-03-29 04:58:12 -0400

    Primitive caching mechanism for Board

--- app/models/Discussion.php
+++ app/models/Discussion.php
@@ -16,16 +16,21 @@ class Discussion extends \Model {
 	 * Support level2 list only.
 	 */
 	function listById($id) {
-		$result = self::queryDb("SELECT * FROM discussions WHERE id=:id OR parent_id=:id ORDER BY created_at ASC;", array(":id" => $id));
+		if ($this->cache->exists("discussion_id_" . $id))
+			return $this->cache->get("discussion_id_" . $id);
+		
+		$result = $this->queryDb("SELECT * FROM discussions WHERE id=:id OR parent_id=:id ORDER BY created_at ASC;", array(":id" => $id));
 		$ret = array("count" => 0);
 		if (empty($result) or count($result) == 0) return $ret;
+		
 		$ret["discussions"] = $result;
 		$ret["count"] = count($result);
+		$this->cache->set("discussion_id_" . $id, $ret, static::DISCUSSION_CACHE_TTL);
 		return $ret;
 	}
 	
 	function listByBoardId($id) {
-		$result = self::queryDb("SELECT id FROM discussions WHERE board_id=:id AND parent_id=0 ORDER BY created_at DESC, pin DESC;", array(":id" => $id));
+		$result = $this->queryDb("SELECT id FROM discussions WHERE board_id=:id AND parent_id=0 ORDER BY created_at DESC, pin DESC;", array(":id" => $id));
 		if (empty($result) or count($result) == 0) return null;
 		
 		$discussions = array();
@@ -37,7 +42,7 @@ class Discussion extends \Model {
 	
 	function create($parent_id, $user_id, $board_id, $discussion, $body){
 		$created_at = date("Y-m-d H:i:s");
-		self::queryDb(
+		$this->queryDb(
 			"INSERT INTO discussions (parent_id, user_id, board_id, discussion, body, created_at, last_update_at) " .
 			"VALUES (:parent_id, :user_id, :board_id, :discussion, :body, :last_update_at, :last_update_at);",
 			array(
@@ -50,7 +55,7 @@ class Discussion extends \Model {
 			)
 		);
 		
-		$result = self::queryDb(
+		$result = $this->queryDb(
 			"SELECT id FROM discussions WHERE parent_id=:parent_id AND user_id=:user_id AND board_id=:board_id AND created_at=:created_at LIMIT 1;", 
 			array(
 				':parent_id' => $parent_id,
@@ -66,12 +71,14 @@ class Discussion extends \Model {
 	}
 	
 	function delete($id) {
-		self::queryDb("DELETE FROM discussions WHERE id=:id OR parent_id=:id LIMIT 1;", array(":id" => $id));
+		$this->queryDb("DELETE FROM discussions WHERE id=:id OR parent_id=:id LIMIT 1;", array(":id" => $id));
+		if ($this->cache->exists("discussion_id_" . $id))
+			$this->cache->clear("discussion_id_" . $id);
 	}
 	
 	function save(&$discussion_info) {
 		$discussion_info["last_update_at"] = date("Y-m-d H:i:s");
-		self::queryDb("UPDATE discussions " .
+		$this->queryDb("UPDATE discussions " .
 			"SET parent_id=:parent_id, user_id=:user_id, board_id=:board_id, discussion=:discussion, body=:body, last_update_at=:last_update_at ".
 			"WHERE id=:id;",
 			array(
@@ -84,5 +91,7 @@ class Discussion extends \Model {
 				":last_update_at" => $discussion_info["last_update_at"],
 			)
 		);
+		if ($this->cache->exists("discussion_id_" . $id))
+			$this->cache->clear("discussion_id_" . $id);
 	}
 }
