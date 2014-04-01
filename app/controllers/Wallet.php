62b1794d9e4e4b40d3060702b9185e656edf9fd3
Xiangyu Bu <xybu92@live.com>
2014-04-01 18:28:18 -0400

    Some more classes.

---
+++ app/controllers/Wallet.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The controller for Wallet class, also handles api for records.
+ */
+
+namespace controllers;
+
+class Wallet extends \Controller {
+	
+	function __construct() {
+		parent::__construct();
+	}
+	
+	function api_create($base) {
+	}
+	
+	function api_edit($base) {
+	}
+	
+	function api_delete($base) {
+	}
+	
+	function api_list($base) {
+	}
+	
+	function api_addRecord($base) {
+	}
+	
+	function api_deleteRecord($base) {
+	}
+	
+}
