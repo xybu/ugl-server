d20b676a896229025f4e103275d516efb7d02bb5
Xiangyu Bu <xybu92@live.com>
2014-04-07 23:00:30 -0400

    Stubs for Shelf

---
+++ app/controllers/Shelf.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace controllers;
+
+class Shelf extends \Controller {
+	
+	function __construct() {
+		parent::__construct();
+	}
+	
+	
---
+++ app/models/Shelf.php
@@ -0,0 +1,6 @@
+<?php
+namespace models;
+
+class Shelf extends \Model {
+
+}
